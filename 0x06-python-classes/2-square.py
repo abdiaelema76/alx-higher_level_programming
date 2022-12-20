afdf140fbd4b9e78fa3307a956e93f51bed5cebc
Abdia Elema <gababoabdiaelema@gmail.com>
2022-12-20 12:07:35 -0800

    commits

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,16 +2,17 @@
 """A module for working with squares.
 """
 
+
 class square:
     """Check for TypeError and ValueError
     """
     def __init__(self, size=0):
         """Initializes a Square with a given size.
-	"""
-	if not isinstance(size, int):
-	     raise TypeError('size must be an integer')
-	 else:
-	      if size < 0:
-	          raise ValueError('size must be >= 0')
-              else:
-	          self.__size = size
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        else:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
