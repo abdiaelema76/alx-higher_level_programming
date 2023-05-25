b475d5cfcdfdfc08fa23fec298fa56ed59056401
Abdia Elema <gababoabdiaelema@gmail.com>
2023-05-25 03:13:27 -0700

    tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) - 1 != 3:
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
 
         if sys.argv[2] not in list(ops.keys()):
             print("Unkonwn operator. Available operators: +, -, *, and /")
 
         sys.exit(1)
-        
+
         a = int(sys.argv[1])
         b = int(sys.argv[3])
 
