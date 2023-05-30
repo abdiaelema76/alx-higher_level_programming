8e7833d9f7ef6021d02a6c4666b2b3687c2e356e
Abdia Elema <gababoabdiaelema@gmail.com>
2023-05-30 12:18:54 -0700

    tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing the square  class
+        Args: size - represents the size of the square defined
+        """
+        self.__size = size
