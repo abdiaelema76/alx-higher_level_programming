5c0093ad2eb5cf52ce3523419f8e36a0153b54ac
Abdia Elema <gababoabdiaelema@gmail.com>
2023-05-31 08:10:42 -0700

    tasks

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_l = []
+
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_l.append(True)
+        else:
+            new_l.append(False)
+    return new_l
