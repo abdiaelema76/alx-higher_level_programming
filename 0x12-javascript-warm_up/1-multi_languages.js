351a31bd2bc00ef100c059dd405b5af400bfd2a6
Abdia Elema <gababoabdiaelema@gmail.com>
2023-08-14 12:14:12 -0700

    concatenation

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// declaring variables
+let c = 'C is fun';
+let python = 'Python is cool';
+let js = 'JavaScript is amazing';
+
+// displaying output
+console.log(c + '\n' + python + '\n' + js);
