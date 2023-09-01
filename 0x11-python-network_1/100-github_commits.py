72ea8d3522355322891f547e59efa8a803e770c1
abdiaelema76 <gababgoabdiaelema@gmail.com>
2023-09-02 00:58:48 +0300

    tasks

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to solve this challenge.
+The Holberton School staff evaluates candidates applying
+for a back-end position with multiple technical challenges"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
